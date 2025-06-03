5b1b6e5e2ed0ce6851adef7ba633d81ad3d5c3c8
juan miguel <miguel.rojas@jala.university>
2025-06-03 18:18:37 -0500

    implement the missing methods to complete the CRUD

--- src/main/java/DAO/PersonaDao.java
+++ src/main/java/DAO/PersonaDao.java
@@ -1,11 +1,8 @@
 package DAO;
 
-import Controller.Persona;
+import Domain.Persona;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +14,7 @@ public class PersonaDao {
         this.connection = ConnectionDB.getConnection();
     }
 
+    //TODO: READ METHOD
     public List<Persona> getPersonas() {
         List<Persona> personaList = new ArrayList<>();
         String sql = "SELECT Id, Nombre, Edad  FROM personas ";
@@ -36,6 +34,49 @@ public class PersonaDao {
             System.out.println("error from getPersonas method = " + e.getMessage());
         }
         return personaList;
+    }
+
+    //TODO: CREATE PERSONA METHOD
+    public void createPersona(Persona persona) throws SQLException {
+        String sql = "INSERT INTO personas (Nombre, Edad) VALUES (?,?) ";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, persona.getNombre());
+            statement.setInt(2, persona.getEdad());
+            statement.executeUpdate();
+        }
+    }
+
+    //TODO: UPDATE PERSONA METHOD
+    public void updatePersona(Persona persona) throws SQLException {
+        String sql = "UPDATE personas SET Nombre = ?, Edad = ? WHERE ID = ? ";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, persona.getNombre());
+            statement.setInt(2, persona.getEdad());
+            statement.setInt(3, persona.getId());
+            statement.executeUpdate();
+        }
+    }
+
+    //TODO: DELETE PERSONA METHOD
+    public void deletePersona(Persona persona) throws SQLException {
+        String sql = "DELETE FROM personas WHRE id = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, persona.getId());
+
+            int affectedRows = statement.executeUpdate();
+
+            if (affectedRows > 0) {
+                System.out.println("la persona con el Id " + persona.getId() +
+                        ">>" + persona.getNombre() + " fue eliminada");
+            } else {
+                System.out.println("no se encontro ninguna persona con el id " + persona.getId());
+            }
+        }
     }
 
 }
