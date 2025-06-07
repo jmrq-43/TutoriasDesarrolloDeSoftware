c62decb7eb5accacaf5bdf923fa31c7f5f8833fa
juan miguel <miguel.rojas@jala.university>
2025-06-07 16:06:30 -0500

    implement print  menu method

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,5 +1,45 @@
 package view;
 
+import controller.PersonController;
+import dao.PersonaDao;
+
+import java.util.Scanner;
+
 public class View {
+    private Scanner scanner = new Scanner(System.in);
+    private PersonController personController  = new PersonController();
+
+    private void printMenu() {
+        System.out.println("""
+                Bienvenido \n
+                1. crear una nueva persona 
+                2. mostrar personas 
+                3. editar una persona  
+                4. eliminar una persona                 
+                5. salir.                 
+                """);
+    }
+
+    private void printPersons() {
+        PersonaDao.getPersonas().forEach(persona -> {
+        });
+    }
+
+    public void slection() {
+        printMenu();
+        int option = Integer.parseInt(scanner.nextLine());
+
+        switch (option) {
+            case  1:
+                personController.printPerson();
+
+        }
+    }
+
+    public void init() {
+        slection();
+    }
+
+
 
 }
