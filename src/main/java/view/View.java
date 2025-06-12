9f2d292aa2ca1814bf48738cdf87a5087a702f31
juan miguel <miguel.rojas@jala.university>
2025-06-12 17:55:22 -0500

    implement switch case for all cases

--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,40 +1,54 @@
 package view;
 
 import controller.PersonController;
-import dao.PersonaDao;
 
 import java.util.Scanner;
 
 public class View {
     private Scanner scanner = new Scanner(System.in);
-    private PersonController personController  = new PersonController();
+    private PersonController personController = new PersonController();
+    private String name;
+    private int age;
 
     private void printMenu() {
-        System.out.println("""
-                Bienvenido \n
+        System.out.print("""
                 1. mostrar Personas 
                 2. agregar Persona
-                3. editar una persona  
-                4. eliminar una persona                 
-                5. salir.                 
+                3. editar una person  
+                4. eliminar una person                 
+                5. salir.    
+                \n             
                 """);
     }
 
     public void slection() {
-        printMenu();
-        int option = Integer.parseInt(scanner.nextLine());
+        boolean exit = false;
 
-        switch (option) {
-            case  1:
-                personController.printPerson();
+        while (!exit) {
 
+            printMenu();
+            int option = Integer.parseInt(scanner.nextLine());
+
+            switch (option) {
+                case 1:
+                    personController.printPerson();
+                    break;
+                case 2:
+                    personController.addPerson();
+                    break;
+                case 3:
+                    personController.updatePerson();
+                    break;
+                case 4:
+                    personController.deletePerson();
+                    break;
+                case 5:
+                    exit = true;
+            }
         }
     }
 
     public void init() {
         slection();
     }
-
-
-
 }
